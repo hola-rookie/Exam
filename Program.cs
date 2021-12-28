9ede8c0a7368dd559f50d45f1cc24fa4fd330aa3
User <User@LAPTOP-80OKVSLE>
2021-12-28 09:57:42 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // a => 1
+            // b => 2
+            // c => 3
+            // d => 跳過
+            // e => 結束
+            // ef => c
+            // 其餘字元不處理
+           // Console.WriteLine(Convert(null) == "");
+            //Console.WriteLine(Convert("") == "");
+            Console.WriteLine(Convert("abc") == "123");
+            Console.WriteLine(Convert("abcd") == "123");
+            Console.WriteLine(Convert("abcde") == "123");
+            Console.WriteLine(Convert("abcdeaa") == "123");
+            Console.WriteLine(Convert("abcdefg") == "123cg");
+        }
+
+        static string Convert(string source)
+
+        {
+            // 請將處理邏輯寫在這裡
+
+            Console.WriteLine();
+            //char[] raw_Data =
+            //char[] compare_Data = ;
+            /*
+            for (int i=0; i < source.Length; i++)
+            {
+                string Raw_Data = source;
+
+                if (Raw_Data[i] == 'a' && (Raw_Data[i + 1] != 'a' || Raw_Data[i - 1] == 'a'))
+                {
+                    continue;
+                }
+                else if (Raw_Data[i] == 'a')
+                {
+                    Console.Write("1");
+                }
+                else if (Raw_Data[i] == 'b')
+                {
+                    Console.Write("2");
+                }
+                else if (Raw_Data[i] == 'c' && Raw_Data[i + 1] == 'f')
+                {
+                    Console.Write("C");
+                }
+                else if (Raw_Data[i] == 'c')
+                {
+                    Console.Write("3");
+                }
+                else if (Raw_Data[i] == 'd')
+                {
+                    break;
+                }
+                else 
+                {
+                    break;
+                } */
+            }
+            return source;
+        }
+    }
+}
+//把source裡的字串一個一個做讀取判斷
+/*
+ for(讀取所有source字元數量跟內容)
+    if      (讀到a)
+        輸出1
+    else if (讀到b)
+        輸出2
+    else if (讀到c && 下一個為f)
+        輸出c
+    else if (讀到c)
+        輸出3
+    eles if (讀到g)
+        輸出g
+    else
+        輸出""
+ 
+ */
