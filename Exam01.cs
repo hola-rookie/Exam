43b2f2291ba30bc45a6278715d4360290bcaf140
Martin Hong <simis00916@outlook.com>
2021-12-30 15:43:10 +0800

    完成版

--- Exam01.cs
+++ Exam01.cs
@@ -39,70 +39,64 @@ namespace Exam
             // d => 跳過
             // e => 結束
             // ef => c
-            // 其餘字元不處理
+            // 其餘字元輸出原始字元
 
             // 請將處理邏輯寫在這裡
 
-            string[] Result_array = new string[source.Length];
+            string SingleResult = "";
+            string Result = "" ;
 
-            if (source == null)  //排除source非字元的情況
+            if (source == null)  //排除source為無東西的情況
             {
-                Result_array = null;
+                Result = "";
             }
-            else if (source == "") //排除source非字元的情況
+            else if (source == "") //排除source空字元的情況
             {
-                Result_array = null;
+                Result = "";
             }
             else
             {
-                string Add_A_to_source = string.Format("{0}A", source); //增加一個字元讓後面的條件判斷不會跳出"超過陣列範圍"                                                       // Console.WriteLine(Add_A_to_source);
-
+                string Add_A_to_source = string.Format("{0}A", source); //增加一個字元讓後面的條件判斷不會跳出"超過陣列範圍"
+                
                 for (int i = 0; i < source.Length; i++)
                 {
                     if (Add_A_to_source[i] == 'a')
                     {
-                        //Console.Write("1");
-                        string Result = "1";
-                        Result_array[i] = Result;
+                        SingleResult = "1";
                     }
                     else if (Add_A_to_source[i] == 'b')
                     {
-                        //Console.Write("2");
-                        string Result = "2";
-                        Result_array[i] = Result;
+                        SingleResult = "2";
                     }
                     else if (Add_A_to_source[i] == 'c')
                     {
-                        //Console.Write("3");
-                        string Result = "3";
-                        Result_array[i] = Result;
-                    }
-                    else if (Add_A_to_source[i] == 'g')
-                    {
-                        //Console.Write("g");
-                        string Result = "g";
-                        Result_array[i] = Result;
+                        SingleResult = "3";
                     }
                     else if (Add_A_to_source[i] == 'd')
                     {
                         continue;
                     }
-                    else if (Add_A_to_source[i] == 'e' && Add_A_to_source[i + 1] == 'f')
+                    else if (Add_A_to_source[i] == 'e' && Add_A_to_source[i + 1] == 'f') //當下字元為e且後一個為f時，條件成立
                     {
-                        Console.Write("c");
-                        string Result = "c";
-                        Result_array[i] = Result;
+                        SingleResult = "c";
+                    }
+                    else if (Add_A_to_source[i] == 'f' && Add_A_to_source[i - 1] == 'e') //當下字元為f且前一個為e時，條件成立
+                    {
+                        SingleResult = null;
                     }
                     else if (Add_A_to_source[i] == 'e')
                     {
                         break;
                     }
-
-                    
+                    else
+                    {
+                        SingleResult = char.ToString(source[i]); // source[i] 是字元，要改成字串格式存進SigleResult
+                    }
+                    Result += SingleResult; 
                 }
                 Console.WriteLine("");
             }
-            return Result_array;
+            return Result;
         }
     }
 }
